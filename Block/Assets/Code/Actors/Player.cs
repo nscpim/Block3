baec645fcdb7c457ec94f02bd690959c1858fa0f
NickdeFries <97888153+NickdeFries@users.noreply.github.com>
2022-03-22 16:04:00 +0100

    created movement

--- Block/Assets/Code/Actors/Player.cs
+++ Block/Assets/Code/Actors/Player.cs
@@ -6,6 +6,20 @@ public class Player : Actor
 {
     public int needsBar { get; private set; }
     private RaycastHit hit;
+    private enum RotationAxes { mouseXandY = 0, mouseX = 1, mouseY = 2 }
+    private RotationAxes axes = RotationAxes.mouseXandY;
+    private float sensitivityX = 5f;
+    private float sensitivityY = 5f;
+    private float minimumY = -60F;
+    private float maximumY = 60F;
+    public float gravity = 40.0f;
+    private float rotationY = 0f;
+    private Vector3 moveDirection = Vector3.zero;
+    private float turner;
+    private float looker;
+    public float sensitivity;
+    private float speed = 6.0F;
+
 
     public void Awake()
     {
@@ -15,17 +29,21 @@ public class Player : Actor
     // Start is called before the first frame update
     public void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
 
     }
 
     // Update is called once per frame
     public void Update()
     {
+
+        movement();
+        CameraMove();
         var energyManager = GameManager.GetManager<EnergyManager>();
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            GameManager.GetManager<UIManager>().ShowTempUI(5, "Test", 400, 400);
-        }
+      //  if (Input.GetKeyDown(KeyCode.A))
+      //  {
+      //      GameManager.GetManager<UIManager>().ShowTempUI(5, "Test", 400, 400);
+     //   }
         if (Input.GetKeyDown(KeyCode.T))
         {
             if (energyManager.eventTimer.isActive)
@@ -56,4 +74,65 @@ public class Player : Actor
             }
         }
     }
+    public void movement() 
+    {
+        CharacterController controller = GetComponent<CharacterController>();
+
+        if (controller.isGrounded)
+        {
+
+            moveDirection = new Vector3(Input.GetAxis("Horizontal") * 0.4f, 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+
+            moveDirection *= speed;
+
+        }
+
+            turner = Input.GetAxis("Mouse X") * sensitivity;
+            looker = -Input.GetAxis("Mouse Y") * sensitivity;
+            if (turner != 0)
+            {
+
+                transform.localEulerAngles += new Vector3(0, turner, 0);
+            }
+            if (looker != 0)
+            {
+
+                transform.localEulerAngles += new Vector3(looker, 0, 0);
+            }
+
+            moveDirection.y -= gravity * Time.deltaTime;
+            controller.Move(moveDirection * Time.deltaTime);
+
+
+        
+    }
+    private void CameraMove()
+    {
+        if (axes == RotationAxes.mouseXandY)
+        {
+            float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+            transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+
+
+        }
+        else if (axes == RotationAxes.mouseX)
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+        }
+        else
+        {
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+            transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
+        }
+    }
+
+   
 }
+
