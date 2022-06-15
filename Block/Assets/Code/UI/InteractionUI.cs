a57cc0fce6002b0756aac42e7957c15b107212c8
BentJarmo <96057964+BentJarmo@users.noreply.github.com>
2022-06-15 16:38:58 +0200

    Added energy usgage to some objects

--- Block/Assets/Code/UI/InteractionUI.cs
+++ Block/Assets/Code/UI/InteractionUI.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "InteractionUI")]
 
 public class InteractionUI : ScriptableObject {
+    [TextArea(4, 10)]
     public string text;
     public bool firstTime;
    
